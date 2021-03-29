bd3b991f3f1b9dc92667d9cfdb12d7c6e209e5e7
sachinnUW <sachinn@uw.edu>
2021-03-28 18:19:29 -0700

    Added bash script to run all files and changed the name of lena-handover-many.cc

---
+++ run.sh
@@ -0,0 +1,9 @@
+./waf --run "lena-handover-many --simTime=10 timeToTrigger=40 hysteresis=1" > HO_40_1.txt
+./waf --run "lena-handover-many --simTime=10 timeToTrigger=40 hysteresis=3" > HO_40_3.txt
+./waf --run "lena-handover-many --simTime=10 timeToTrigger=40 hysteresis=5" > HO_40_5.txt
+./waf --run "lena-handover-many --simTime=10 timeToTrigger=80 hysteresis=1" > HO_80_1.txt
+./waf --run "lena-handover-many --simTime=10 timeToTrigger=80 hysteresis=3" > HO_80_3.txt
+./waf --run "lena-handover-many --simTime=10 timeToTrigger=80 hysteresis=5" > HO_80_5.txt
+./waf --run "lena-handover-many --simTime=10 timeToTrigger=256 hysteresis=1" > HO_256_1.txt
+./waf --run "lena-handover-many --simTime=10 timeToTrigger=256 hysteresis=3" > HO_256_3.txt
+./waf --run "lena-handover-many --simTime=10 timeToTrigger=256 hysteresis=5" > HO_256_5.txt
